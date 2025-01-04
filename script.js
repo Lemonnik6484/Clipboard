0d8fad0aa03f1c238b38ce4ae849dbb9f6e5a4f9
Lemonnik6484 <LemonGamer6484@gmail.com>
2025-01-04 13:49:49 +0100

    PWA test

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js')
+        .then(() => console.log('Service Worker registered'))
+        .catch(err => console.error('Service Worker registration failed:', err));
+}
+
 let data = {
     "page1": {}
 };
